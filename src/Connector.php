91e34845168e98e5bbd1f5735440ece448ee6066
Sebastian Bach Andersen <ostehandler@hotmail.dk>
2023-03-10 16:14:42 +0100

    refactor

--- src/Connector.php
+++ src/Connector.php
@@ -25,17 +25,6 @@ class Connector
      */
     public array $dnsIDNCommands = ['AddDNSZone', 'ModifyDNSZone', 'QueryDNSZoneRRList'];
 
-    public array $curlOpts = [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 20,
-        CURLOPT_HEADER => false,
-        CURLOPT_SSL_VERIFYPEER => true,
-        CURLOPT_HTTPHEADER => [
-            'Expect:',
-            'Content-type: text/html; charset=UTF-8'
-        ]
-    ];
-
     public function __construct(string $username, string $password, bool $test = false)
     {
         $this->username = $username;
@@ -45,15 +34,17 @@ class Connector
 
     public function formatDomainArg(string $command, array $args = []): array
     {
-        if (function_exists('idn_to_ascii')) {
-            // IDN Conversion
-            if (in_array($command, $this->domainIDNCommands)) {
-                $idn = idn_to_ascii($args['domain'], IDNA_DEFAULT);
-                $args['domain'] = $idn ?: $args['domain'];
-            } elseif (in_array($command, $this->dnsIDNCommands)) {
-                $idn = idn_to_ascii($args['dnszone'], IDNA_DEFAULT);
-                $args['dnszone'] = $idn ?: $args['dnszone'];
-            }
+        // IDN Conversion
+        if (in_array($command, $this->domainIDNCommands)) {
+
+            $idn = idn_to_ascii($args['domain']);
+            $args['domain'] = $idn ?: $args['domain'];
+
+        } elseif (in_array($command, $this->dnsIDNCommands)) {
+
+            $idn = idn_to_ascii($args['dnszone']);
+            $args['dnszone'] = $idn ?: $args['dnszone'];
+
         }
 
         return $args;
@@ -66,12 +57,12 @@ class Connector
      * @return mixed
      * @throws Exception
      */
-    public function validateResult($result, string $command, array $args = [])
+    public function validateResult($result, string $command, array $args = []): mixed
     {
-        if ((preg_match('/^2/', $result['code']))) { // Successful Return Codes (2xx), return the results.
+        if ((str_starts_with($result['code'], '2'))) { // Successful Return Codes (2xx), return the results.
             $this->retry = false;
             return $result;
-        } elseif ((preg_match('/^4/', $result['code'])) && !$this->retry) { // Temporary Error Codes (4xx), we do a retry .
+        } elseif ((str_starts_with($result['code'], '4')) && !$this->retry) { // Temporary Error Codes (4xx), we do a retry .
             $this->retry = true;
             sleep(5);
             return $this->request($command, $args);
@@ -80,7 +71,13 @@ class Connector
         }
     }
 
-    public function request(string $command, $args = [])
+    /**
+     * @param string $command
+     * @param array $args
+     * @return mixed
+     * @throws Exception
+     */
+    public function request(string $command, array $args = [])
     {
         $args = $this->formatDomainArg($command, $args);
 
@@ -121,7 +118,12 @@ class Connector
         return $this->validateResult($result, $command, $requestArgs);
     }
 
-    private function processResponse($response)
+    /**
+     * @param $response
+     * @return array|array[]
+     * @throws Exception
+     */
+    private function processResponse($response): array
     {
         if (is_array($response)) {
             return $response;
@@ -132,16 +134,16 @@ class Connector
         }
 
         $hash = array("property" => array());
-        $rlist = explode("\n", $response);
-        foreach ($rlist as $item) {
+        $responseList = explode("\n", $response);
+        foreach ($responseList as $item) {
             if (preg_match("/^([^\\=]*[^\t\\= ])[\t ]*=[\t ]*(.*)\$/", $item, $m)) {
-                list(, $attr, $value) = $m;
-                $value = preg_replace("/[\t ]*\$/", "", $value);
+                [,$attr, $value] = $m;
+                $value = rtrim($value, " \t");
                 if (preg_match("/^PROPERTY\\[([^\\]]*)\\]/i", $attr, $m)) {
                     $prop = strtolower($m[1]);
                     $prop = preg_replace("/\\s/", "", $prop);
                     if (in_array($prop, array_keys($hash["property"]))) {
-                        array_push($hash["property"][$prop], $value);
+                        $hash["property"][$prop][] = $value;
                     } else {
                         $hash["property"][$prop] = array($value);
                     }
